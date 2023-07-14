c508b456d8d37a64086c7ef2afb447377a278213
root <hishamislam68@gmail.com>
2023-07-14 15:33:21 -0700

    alx

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,40 +1,39 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Print all possible combinations of two digits.
+ * Description: Print all possible different combinations of 3 digits.
  * Numbers must be separated by commas and a space.
- * 01 and 10 are considered as the same combination of the two digits.
- * Print only the smallest combination of two digits.
- * Numbers should be printed in ascending order, with two digits.
- * You can only use `putchar`.
- * You can only use `putchar` up to 5 times.
- * You are not allowed to use any variable of type `char`.
+ * The 3 digits must be different.
+ * 012, 120, 102, 021, 201, 210 are considered the same combination.
+ * print only the smallest combination of 3 digits.
+ * Numbers should be printed in ascending order.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 6 times.
+ * You are not allowed to use any variables of type `char`.
  * Return: 0
  */
 int main(void)
 {
-	int a, b, c;
+	int a, b, c, d;
 
-	a = 0;
-
-	while (a < 100)
+	for (a = 0; a < 1000; a++)
 	{
-		b = a % 10; /* singles digit */
-		c = a / 10; /* doubles digit */
+		b = a / 100; /* hundreds */
+		c = (a / 10) % 10; /* tens */
+		d = a % 10; /* singles */
 
-		if (c < b)
+		if (b < c && c < d)
 		{
-			putchar(c + '0');
 			putchar(b + '0');
+			putchar(c + '0');
+			putchar(d + '0');
 
-			if (a < 89)
+			if (a < 700)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
-
-		a++;
 	}
 	putchar('\n');
 
