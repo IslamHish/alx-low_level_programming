fda35451ed52bd9b45a681c5ea1d48b250d10d7d
root <hishamislam68@gmail.com>
2023-07-18 16:42:24 -0700

    alx

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
+ * main - Prints "putchar" followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
